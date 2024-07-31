3512de44077f1375e29d12eb5ca4263de8b30b3e
L30AM <147782782+L30AM@users.noreply.github.com>
2024-07-30 21:49:17 -0600

    update status_dhcp.sh

--- Scripts/status_dhcp.sh
+++ Scripts/status_dhcp.sh
@@ -1,1 +1,68 @@
 #!/bin/bash
+
+# COLORS
+BLUE='\033[0;1;34;94m'
+PURPLE='\033[0;1;35;95m'
+ORANGE='\033[0;1;33;93m'
+RED='\033[0;31m'
+GREEN='\033[0;0;32;92m'
+GREEN2='\033[0;32m'
+PINK='\033[1;36m'
+YELLOW='\033[0;33m'
+WHITE='\033[1;37m'
+NOCOLOR='\033[0m'
+DHCPCOLOR='\033[1;33m'
+
+#Utils
+source Utils/progress_bar.sh
+
+show_title() {
+    clear
+    bash Utils/show_title.sh $DHCPCOLOR
+}
+
+show_message() {
+    local c=$1
+    local message=$2
+    local color=$3
+    echo -e " ${BLUE}[${color}${c}${BLUE}]${color} ${message}${NOCOLOR}"
+}
+
+check_status() {
+    show_title
+    echo -e "${BLUE}Checking DHCP service status...${NOCOLOR}"
+    systemctl status dhcpd
+    echo -e "${BLUE}----------------------------------------------------------------------------------${NOCOLOR}"
+    echo -e "${BLUE}Press any key to return to the main menu.${NOCOLOR}"
+    read -r -n 1
+}
+
+show_logs() {
+    show_title
+    echo -e "${BLUE}Showing DHCP leases log...${NOCOLOR}"
+    cat /var/lib/dhcpd/dhcpd.leases
+    echo -e "${BLUE}----------------------------------------------------------------------------------${NOCOLOR}"
+    echo -e "${BLUE}Press any key to return to the main menu.${NOCOLOR}"
+    read -r -n 1
+}
+
+main_menu() {
+    while [ true ]; do
+        show_title
+        echo ""
+        echo -e " ${BLUE}[${YELLOW}1${BLUE}]${NOCOLOR} Check DHCP Service Status"
+        echo -e " ${BLUE}[${YELLOW}2${BLUE}]${NOCOLOR} Show DHCP Leases Logs"
+        echo -e " ${BLUE}[${YELLOW}3${BLUE}]${NOCOLOR} Exit"
+        echo ""
+        echo -ne " ${BLUE}Enter an option ${YELLOW}\$${BLUE}>:${NOCOLOR} "
+        read -r op
+        case $op in
+            1) check_status ;;
+            2) show_logs ;;
+            3) break ;;
+            *) show_message "X" "Invalid option." $RED ;;
+        esac
+    done
+}
+
+main_menu
