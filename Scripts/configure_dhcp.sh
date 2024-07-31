898266d25923800424ea1beb747eec5272cbf22b
gsusL <gsuslanda@gmail.com>
2024-07-30 20:58:45 -0600

    Update configure_dhcp.sh

--- Scripts/configure_dhcp.sh
+++ Scripts/configure_dhcp.sh
@@ -19,6 +19,8 @@ DEFAULT_INTERFACE_CONF="/etc/sysconfig/dhcpd"
 dhcp_changed=0
 interface_changed=0
 
+# Utils
+source Utils/progress_bar.sh
 
 show_title() {
     clear
@@ -30,22 +32,6 @@ show_message() {
     local message=$2
     local color=$3
     echo -e " ${BLUE}[${color}${c}${BLUE}]${color} ${message}${NOCOLOR}"
-}
-
-progress_bar() {
-    local duration=$1
-    local steps=10
-    local interval=$((duration / steps))
-
-    local color=$2
-    for ((i = 0; i <= steps; i++)); do
-        echo -ne "${BLUE} ["
-        for ((j = 0; j < i; j++)); do echo -ne "${color}###"; done
-        for ((j = i; j < steps; j++)); do echo -ne "${NOCOLOR}..."; done
-        echo -ne "${BLUE}] ${color}$((i * 10))${BLUE}%\r"
-        sleep $interval
-    done
-    echo -e "${NOCOLOR}"
 }
 
 read_config() {
