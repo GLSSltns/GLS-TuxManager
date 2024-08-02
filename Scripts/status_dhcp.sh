ba33081087fddee162a2b448abba4b3b9c7113ab
L30AM <147782782+L30AM@users.noreply.github.com>
2024-08-01 23:53:18 -0600

    update status_dhcp.sh

--- Scripts/status_dhcp.sh
+++ Scripts/status_dhcp.sh
@@ -14,42 +14,52 @@ NOCOLOR='\033[0m'
 source Utils/progress_bar.sh
 source Utils/show_message.sh
 
+
 show_title() {
     clear
     bash Utils/show_title.sh $DHCPCOLOR
 }
 
+# Function to check the status of the DHCP service
 check_status() {
     show_title
     echo -e "${MAIN_COLOR}Checking DHCP service status...${NOCOLOR}"
 
+    # Get the status of the DHCP service
     DHCPDSTATUS=$(systemctl status dhcpd)
+
+    # Extract the relevant information from the status
     STATUS=$(echo $DHCPDSTATUS | grep -Po "Active: \K[a-z\(\)_]*")
     PID=$(echo $DHCPDSTATUS | grep -Po "PID: \K[\d]*")
     MEMORY=$(echo $DHCPDSTATUS | grep -Po "Memory: \K[\dA-Z.]*")
     CPU=$(echo $DHCPDSTATUS | grep -Po "CPU: \K[\da-z.]*")
 
+    # Display the extracted information
     echo -e "${MAIN_COLOR}Status: ${NOCOLOR}$STATUS"
     echo -e "${MAIN_COLOR}PID: ${NOCOLOR}$PID"
     echo -e "${MAIN_COLOR}Memory: ${NOCOLOR}$MEMORY"
     echo -e "${MAIN_COLOR}CPU: ${NOCOLOR}$CPU"
     echo -e "${MAIN_COLOR}----------------------------------------------------------------------------------${NOCOLOR}"
     echo -e "${MAIN_COLOR}Press any key to return to the main menu.${NOCOLOR}"
-    read -r -n 1
+    read -r -n 1 #Wait for user input to return to the main menu
 }
 
+# Function to show DHCP leases log
 show_logs() {
     show_title
     echo -e "${MAIN_COLOR}Showing DHCP leases log...${NOCOLOR}"
 
+    # Extract and sort unique hostnames, IP addresses, and MAC addresses from the DHCP leases file
     HOSTNAMES=$(grep -Po "client-hostname \K[\d:a-zA-Z^\"]*" /var/lib/dhcpd/dhcpd.leases | sed 's/"//g' | sort | uniq)
     ADDRESSES=$(grep -Po "lease \K[\d.]*" /var/lib/dhcpd/dhcpd.leases | sort | uniq)
     MACS=$(grep -Po "ethernet \K[\d:a-fA-F]*" /var/lib/dhcpd/dhcpd.leases | sort | uniq)
 
+    # Display the log in a formatted table
     echo -e "${MAIN_COLOR}--------------------------------------------------${NOCOLOR}"
     printf "${MAIN_COLOR}│ ${WHITE}%-10s${MAIN_COLOR} │ ${WHITE}%-15s${MAIN_COLOR} │ ${WHITE}%-17s${MAIN_COLOR} │${NOCOLOR}\n" "HOSTNAME" "ADDRESS" "MAC"
     echo -e "${MAIN_COLOR}--------------------------------------------------${NOCOLOR}"
     
+    # Loop through each entry and display it in the table
     for i in $(seq 1 $(echo "$HOSTNAMES" | wc -l)); do
         HOSTNAME=$(echo "$HOSTNAMES" | sed -n "${i}p")
         ADDRESS=$(echo "$ADDRESSES" | sed -n "${i}p")
@@ -59,26 +69,28 @@ show_logs() {
 
     echo -e "${MAIN_COLOR}----------------------------------------------------------------------------------${NOCOLOR}"
     echo -e "${MAIN_COLOR}Press any key to return to the main menu.${NOCOLOR}"
-    read -r -n 1
+    read -r -n 1 # Wait for user input to return to the main menu
 }
 
+#Function to navigate through options
 main_menu() {
     while [ true ]; do
-        show_title
+        show_title #Display the title
         echo ""
+        #Display menu options
         echo -e " ${MAIN_COLOR}[${DHCPCOLOR}1${MAIN_COLOR}]${NOCOLOR} Check DHCP Service Status"
         echo -e " ${MAIN_COLOR}[${DHCPCOLOR}2${MAIN_COLOR}]${NOCOLOR} Show DHCP Leases Logs"
         echo -e " ${MAIN_COLOR}[${DHCPCOLOR}3${MAIN_COLOR}]${NOCOLOR} Exit DHCP Monitoring"
         echo ""
         echo -ne " ${MAIN_COLOR}Enter an option ${DHCPCOLOR}\$${MAIN_COLOR}>:${NOCOLOR} "
-        read -r op
+        read -r op # Read user input
         case $op in
-            1) check_status ;;
-            2) show_logs ;;
-            3) break ;;
-            *) show_message "X" "Invalid option." $RED ;;
+            1) check_status ;; #Display dhcp service status
+            2) show_logs ;; #Show DHCP logs
+            3) break ;; # Exit the menu loop
+            *) show_message "X" "Invalid option." $RED ;; # Handle invalid input
         esac
     done
 }
 
-main_menu
+main_menu # Start the main menu
