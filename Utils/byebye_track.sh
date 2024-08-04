fb5b256e5abd077544e976283e71c18a5485135e
gsusL <gsuslanda@gmail.com>
2024-08-04 14:39:09 -0600

    Update

--- Utils/byebye_track.sh
+++ Utils/byebye_track.sh
@@ -5,7 +5,7 @@ trap 'byebye_execution' SIGINT
 
 byebye_execution() {
 	echo -ne "\r"
-    show_message "!" "Execution was stopped by the user (^C)!" $RED
+    show_message "!" "Execution was stopped by the user (^C)!" $RED $MAIN_COLOR
     tput sgr0
     stty echo
     stty -igncr
