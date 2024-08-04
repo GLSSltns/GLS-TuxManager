a45a23e94489d31144937f173d12a7ab7c3176fc
gsusL <gsuslanda@gmail.com>
2024-08-03 20:46:22 -0600

    Update

--- Scripts/install_web.sh
+++ Scripts/install_web.sh
@@ -7,7 +7,7 @@ source Utils/validate.sh
 
 # FLAGS
 is_web_installed=0 # Track HTTP installation status
-is_connection=0 # Internet connection
+is_connection=0 # Track internet connection status
 
 # Function to check if HTTP is installed
 is_installed() {
@@ -19,9 +19,43 @@ check_connection() {
     is_connection=$(ping -q -w 1 -c 1 8.8.8.8 > /dev/null && echo 1 || echo 0)
 }
 
+# Function to show the title banner
 show_title() {
     clear
     show_banner $HTTPCOLOR $MAIN_COLOR "WEB Service Installation"
+}
+
+# Function to wait for user input to continue
+wait_for_continue() {
+    echo -e "\n${MAIN_COLOR}----------------------------------------------------------------------------------${NOCOLOR}"
+    echo -ne " ${MAIN_COLOR}Press [${HTTPCOLOR}ANY KEY${MAIN_COLOR}] to continue..."
+    read -r -n 1 -s
+    show_title
+}
+
+# Function to manage package installation, update, or removal
+manage_pkg() {
+    local action=$1
+    local message=$2
+    local command=$3
+
+    show_title
+    echo ""
+
+    check_connection
+    if [ $is_connection -eq 0 ]; then
+        show_message "X" "No internet connection. Cannot proceed.\n" $RED $MAIN_COLOR
+        return
+    fi
+
+    show_message "!" "$message" $YELLOW $MAIN_COLOR
+    sleep 1
+    progress_bar 10 $YELLOW $MAIN_COLOR &
+    eval "$command" > /dev/null 2>&1
+    wait
+    show_message "-" "$message Completed Successfully." $GREEN $MAIN_COLOR
+    wait_for_continue
+    show_menu
 }
 
 # Function to install the HTTP package
@@ -29,27 +63,7 @@ install_pkg() {
     if [ $is_web_installed -eq 1 ]; then
         show_message "!" "HTTP Service Is Already Installed.\n" $YELLOW $MAIN_COLOR
     else
-        show_title  # Display title before installing
-        echo ""
-
-        check_connection
-        if [ $is_connection -eq 0 ]; then
-            show_message "X" "No internet connection. Cannot install HTTP Service.\n" $RED $MAIN_COLOR
-            return
-        fi
-
-        show_message "!" 'Downloading HTTP Package (httpd)...' $YELLOW $MAIN_COLOR
-        sleep 1
-        progress_bar 10 $YELLOW $MAIN_COLOR &  # Show progress bar
-        yum install -y httpd > /dev/null 2>&1  # Install package silently
-        wait  # Wait for progress bar to finish
-        sleep 1
-        show_message "-" "HTTP Service Installed Successfully." $GREEN $MAIN_COLOR
-        echo -e "\n${MAIN_COLOR}----------------------------------------------------------------------------------${NOCOLOR}"
-        echo -ne " ${MAIN_COLOR}Press [${HTTPCOLOR}ANY KEY${MAIN_COLOR}] to continue..."
-        read -r -n 1 -s
-        show_title
-        show_menu
+        manage_pkg "install" "Downloading HTTP Package (httpd)..." "yum install -y httpd"
     fi
 }
 
@@ -59,27 +73,12 @@ remove_pkg() {
         show_title
         echo ""
         show_message "!?" "The HTTP Service Package (httpd) Will Be REMOVED!!" $RED $MAIN_COLOR
-        if prompt_confirmation "Is It Okay?" ; then  # Confirm removal
-            echo ""
-            sleep 2
-            show_message "!" "Removing HTTP Service Package..." $YELLOW $MAIN_COLOR
-            progress_bar 10 $YELLOW $MAIN_COLOR &  # Show progress bar
-            yum remove -y httpd > /dev/null 2>&1  # Remove package silently
-            wait  # Wait for progress bar to finish
-            show_message "-" "HTTP Service Package Removed Successfully." $GREEN $MAIN_COLOR
-            echo -e "\n${MAIN_COLOR}----------------------------------------------------------------------------------${NOCOLOR}"
-            echo -ne " ${MAIN_COLOR}Press [${HTTPCOLOR}ANY KEY${MAIN_COLOR}] to continue..."
-            read -r -n 1 -s
+        if prompt_confirmation "Is It Okay?" ; then
+            manage_pkg "remove" "Removing HTTP Service Package..." "yum remove -y httpd"
         else
-            sleep 1
             show_message "!" "Removal canceled." $YELLOW $MAIN_COLOR
-            echo -e "\n${MAIN_COLOR}----------------------------------------------------------------------------------${NOCOLOR}"
-            echo -ne " ${MAIN_COLOR}Press [${HTTPCOLOR}ANY KEY${MAIN_COLOR}] to continue..."
-            read -r -n 1 -s
+            wait_for_continue
         fi
-        
-        show_title
-        show_menu
     else
         show_message "X" "HTTP Service Is Not Installed, Cannot Remove.\n" $RED $MAIN_COLOR
     fi
@@ -88,28 +87,9 @@ remove_pkg() {
 # Function to update the HTTP package
 update_pkg() {
     if [ $is_web_installed -eq 1 ]; then
-        # Check if an update is available for the HTTP server package
         local is_update_needed=$(yum check-update httpd | grep -q 'httpd' && echo 1 || echo 0)
         if [ $is_update_needed -eq 1 ]; then
-            show_title
-            echo ""
-
-            check_connection
-            if [ $is_connection -eq 0 ]; then
-                show_message "X" "No internet connection. Cannot install HTTP Service.\n" $RED $MAIN_COLOR
-                return
-            fi
-        
-            show_message "!" "Updating HTTP Service Package (httpd)..." $YELLOW $MAIN_COLOR
-            progress_bar 10 $YELLOW $MAIN_COLOR &  # Show progress bar
-            yum update -y httpd > /dev/null 2>&1  # Update package silently
-            wait  # Wait for progress bar to finish
-            show_message "-" "HTTP Service Package Updated Successfully." $GREEN $MAIN_COLOR
-            echo -e "\n${MAIN_COLOR}----------------------------------------------------------------------------------${NOCOLOR}"
-            echo -ne " ${MAIN_COLOR}Press [${HTTPCOLOR}ANY KEY${MAIN_COLOR}] to continue..."
-            read -r -n 1 -s
-            show_title
-            show_menu
+            manage_pkg "update" "Updating HTTP Service Package (httpd)..." "yum update -y httpd"
         else
             show_message "!" "HTTP Service Is Already Up To Date..\n" $YELLOW $MAIN_COLOR
         fi
