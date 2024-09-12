286a2177d182a0f8cc511ba8a30b02dd9a56808d
gsusL <gsuslanda@gmail.com>
2024-09-12 12:31:22 -0600

    Update

--- FTPService/tuxmanager.sh
+++ FTPService/tuxmanager.sh
@@ -81,11 +81,12 @@ show_title() {
 
 show_menu() {
     show_title
-    echo -ne " ${MAIN_COLOR}[${TUXCOLOR}1${MAIN_COLOR}]${NOCOLOR} Install FTP Service\t\t${MAIN_COLOR}[${TUXCOLOR}6${MAIN_COLOR}]${NOCOLOR} Quit"
+    echo -ne "\n ${MAIN_COLOR}[${TUXCOLOR}1${MAIN_COLOR}]${NOCOLOR} Install FTP Service\t\t${MAIN_COLOR}[${TUXCOLOR}6${MAIN_COLOR}]${NOCOLOR} Quit"
     echo -ne "\n ${MAIN_COLOR}[${TUXCOLOR}2${MAIN_COLOR}]${NOCOLOR} Configure FTP Service"
     echo -ne "\n ${MAIN_COLOR}[${TUXCOLOR}3${MAIN_COLOR}]${NOCOLOR} Manage DHCP Service"
     display_not_installed_message "FTP" $is_vsftpd
     echo -ne "\n ${MAIN_COLOR}[${TUXCOLOR}4${MAIN_COLOR}]${NOCOLOR} FTP Service Status"
+    echo ""
     echo ""
 }
 
