2b0187a7eda3140a26db6ef7cc1742edfd8b8fc2
gsusL <gsuslanda@gmail.com>
2024-09-12 11:49:57 -0600

    Update

--- FTPService/Scripts/configure_ftp.sh
+++ FTPService/Scripts/configure_ftp.sh
@@ -29,117 +29,111 @@ show_vsftpd_menu() {
 }
 
 handle_vsftpd_option() {
-    local option=$1
-    local value
-
-	while true; do
-	    show_vsftpd_menu
-	    echo -ne " ${MAIN_COLOR}Enter An Option ${TUXCOLOR}\$${MAIN_COLOR}>: ${NOCOLOR}"
-	    read -r op
-	    if [ -z "$op" ]; then
-	        echo "" > /dev/null
-	    else
-		    case $option in
-		        1)  # Anonymous Enable
-		            value=$(grep -E '^anonymous_enable=' $FTP_CONFIG | cut -d= -f2)
-		            if [ "$value" == "YES" ]; then
-		                sed -i 's/^anonymous_enable=YES/anonymous_enable=NO/' $FTP_CONFIG
-		                show_message "!" "Anonymous Enable has been set to NO." $YELLOW $MAIN_COLOR
-		            else
-		                sed -i 's/^anonymous_enable=NO/anonymous_enable=YES/' $FTP_CONFIG
-		                show_message "!" "Anonymous Enable has been set to YES." $YELLOW $MAIN_COLOR
-		            fi
-		            ;;
-		        2)  # FTPD Banner
-		            echo "Enter new FTPD Banner:"
-		            read new_banner
-		            sed -i "s/^ftpd_banner=.*/ftpd_banner=$new_banner/" $FTP_CONFIG
-		            show_message "!" "FTPD Banner has been updated." $YELLOW $MAIN_COLOR
-		            ;;
-		        3)  # Chroot Local User
-		            value=$(grep -E '^chroot_local_user=' $FTP_CONFIG | cut -d= -f2)
-		            if [ "$value" == "YES" ]; then
-		                sed -i 's/^chroot_local_user=YES/chroot_local_user=NO/' $FTP_CONFIG
-		                show_message "!" "Chroot Local User has been set to NO." $YELLOW $MAIN_COLOR
-		            else
-		                sed -i 's/^chroot_local_user=NO/chroot_local_user=YES/' $FTP_CONFIG
-		                show_message "!" "Chroot Local User has been set to YES." $YELLOW $MAIN_COLOR
-		            fi
-		            ;;
-		        4)  # Chroot List Enable
-		            value=$(grep -E '^chroot_list_enable=' $FTP_CONFIG | cut -d= -f2)
-		            if [ "$value" == "YES" ]; then
-		                sed -i 's/^chroot_list_enable=YES/chroot_list_enable=NO/' $FTP_CONFIG
-		                show_message "!" "Chroot List Enable has been set to NO." $YELLOW $MAIN_COLOR
-		            else
-		                sed -i 's/^chroot_list_enable=NO/chroot_list_enable=YES/' $FTP_CONFIG
-		                show_message "!" "Chroot List Enable has been set to YES." $YELLOW $MAIN_COLOR
-		            fi
-		            ;;
-		        5)  # Allow Writeable Chroot
-		            value=$(grep -E '^allow_writeable_chroot=' $FTP_CONFIG | cut -d= -f2)
-		            if [ "$value" == "YES" ]; then
-		                sed -i 's/^allow_writeable_chroot=YES/allow_writeable_chroot=NO/' $FTP_CONFIG
-		                show_message "!" "Allow Writeable Chroot has been set to NO." $YELLOW $MAIN_COLOR
-		            else
-		                sed -i 's/^allow_writeable_chroot=NO/allow_writeable_chroot=YES/' $FTP_CONFIG
-		                show_message "!" "Allow Writeable Chroot has been set to YES." $YELLOW $MAIN_COLOR
-		            fi
-		            ;;
-		        6)  # Chroot List File
-		            echo "Enter new Chroot List File path:"
-		            read new_chroot_list_file
-		            sed -i "s|^chroot_list_file=.*|chroot_list_file=$new_chroot_list_file|" $FTP_CONFIG
-		            show_message "!" "Chroot List File path has been updated." $YELLOW $MAIN_COLOR
-		            ;;
-		        7)  # LS Recurse Enable
-		            value=$(grep -E '^ls_recurse_enable=' $FTP_CONFIG | cut -d= -f2)
-		            if [ "$value" == "YES" ]; then
-		                sed -i 's/^ls_recurse_enable=YES/ls_recurse_enable=NO/' $FTP_CONFIG
-		                show_message "!" "LS Recurse Enable has been set to NO." $YELLOW $MAIN_COLOR
-		            else
-		                sed -i 's/^ls_recurse_enable=NO/ls_recurse_enable=YES/' $FTP_CONFIG
-		                show_message "!" "LS Recurse Enable has been set to YES." $YELLOW $MAIN_COLOR
-		            fi
-		            ;;
-		        8)  # Listen
-		            value=$(grep -E '^listen=' $FTP_CONFIG | cut -d= -f2)
-		            if [ "$value" == "YES" ]; then
-		                sed -i 's/^listen=YES/listen=NO/' $FTP_CONFIG
-		                show_message "!" "Listen has been set to NO." $YELLOW $MAIN_COLOR
-		            else
-		                sed -i 's/^listen=NO/listen=YES/' $FTP_CONFIG
-		                show_message "!" "Listen has been set to YES." $YELLOW $MAIN_COLOR
-		            fi
-		            ;;
-		        9)  # Listen IPv6
-		            value=$(grep -E '^listen_ipv6=' $FTP_CONFIG | cut -d= -f2)
-		            if [ "$value" == "YES" ]; then
-		                sed -i 's/^listen_ipv6=YES/listen_ipv6=NO/' $FTP_CONFIG
-		                show_message "!" "Listen IPv6 has been set to NO." $YELLOW $MAIN_COLOR
-		            else
-		                sed -i 's/^listen_ipv6=NO/listen_ipv6=YES/' $FTP_CONFIG
-		                show_message "!" "Listen IPv6 has been set to YES." $YELLOW $MAIN_COLOR
-		            fi
-		            ;;
-		        10)  # Use Localtime
-		            value=$(grep -E '^use_localtime=' $FTP_CONFIG | cut -d= -f2)
-		            if [ "$value" == "YES" ]; then
-		                sed -i 's/^use_localtime=YES/use_localtime=NO/' $FTP_CONFIG
-		                show_message "!" "Use Localtime has been set to NO." $YELLOW $MAIN_COLOR
-		            else
-		                sed -i 's/^use_localtime=NO/use_localtime=YES/' $FTP_CONFIG
-		                show_message "!" "Use Localtime has been set to YES." $YELLOW $MAIN_COLOR
-		            fi
-		            ;;
-		        11)  # Go Back
-		            break
-		            ;;
-		        *)  # Invalid option
-		            show_message "!" "Invalid option." $RED $MAIN_COLOR
-		            ;;
-		    esac
-		fi
+    while true; do
+        show_vsftpd_menu
+        echo -ne " ${MAIN_COLOR}Enter An Option ${TUXCOLOR}\$${MAIN_COLOR}>: ${NOCOLOR}"
+        read -r op
+        case $op in
+            1)  # Anonymous Enable
+                value=$(grep -E '^anonymous_enable=' $FTP_CONFIG | cut -d= -f2)
+                if [ "$value" == "YES" ]; then
+                    sed -i 's/^anonymous_enable=YES/anonymous_enable=NO/' $FTP_CONFIG
+                    show_message "!" "Anonymous Enable has been set to NO." $YELLOW $MAIN_COLOR
+                else
+                    sed -i 's/^anonymous_enable=NO/anonymous_enable=YES/' $FTP_CONFIG
+                    show_message "!" "Anonymous Enable has been set to YES." $YELLOW $MAIN_COLOR
+                fi
+                ;;
+            2)  # FTPD Banner
+                echo "Enter new FTPD Banner:"
+                read new_banner
+                sed -i "s/^ftpd_banner=.*/ftpd_banner=$new_banner/" $FTP_CONFIG
+                show_message "!" "FTPD Banner has been updated." $YELLOW $MAIN_COLOR
+                ;;
+            3)  # Chroot Local User
+                value=$(grep -E '^chroot_local_user=' $FTP_CONFIG | cut -d= -f2)
+                if [ "$value" == "YES" ]; then
+                    sed -i 's/^chroot_local_user=YES/chroot_local_user=NO/' $FTP_CONFIG
+                    show_message "!" "Chroot Local User has been set to NO." $YELLOW $MAIN_COLOR
+                else
+                    sed -i 's/^chroot_local_user=NO/chroot_local_user=YES/' $FTP_CONFIG
+                    show_message "!" "Chroot Local User has been set to YES." $YELLOW $MAIN_COLOR
+                fi
+                ;;
+            4)  # Chroot List Enable
+                value=$(grep -E '^chroot_list_enable=' $FTP_CONFIG | cut -d= -f2)
+                if [ "$value" == "YES" ]; then
+                    sed -i 's/^chroot_list_enable=YES/chroot_list_enable=NO/' $FTP_CONFIG
+                    show_message "!" "Chroot List Enable has been set to NO." $YELLOW $MAIN_COLOR
+                else
+                    sed -i 's/^chroot_list_enable=NO/chroot_list_enable=YES/' $FTP_CONFIG
+                    show_message "!" "Chroot List Enable has been set to YES." $YELLOW $MAIN_COLOR
+                fi
+                ;;
+            5)  # Allow Writeable Chroot
+                value=$(grep -E '^allow_writeable_chroot=' $FTP_CONFIG | cut -d= -f2)
+                if [ "$value" == "YES" ]; then
+                    sed -i 's/^allow_writeable_chroot=YES/allow_writeable_chroot=NO/' $FTP_CONFIG
+                    show_message "!" "Allow Writeable Chroot has been set to NO." $YELLOW $MAIN_COLOR
+                else
+                    sed -i 's/^allow_writeable_chroot=NO/allow_writeable_chroot=YES/' $FTP_CONFIG
+                    show_message "!" "Allow Writeable Chroot has been set to YES." $YELLOW $MAIN_COLOR
+                fi
+                ;;
+            6)  # Chroot List File
+                echo "Enter new Chroot List File path:"
+                read new_chroot_list_file
+                sed -i "s|^chroot_list_file=.*|chroot_list_file=$new_chroot_list_file|" $FTP_CONFIG
+                show_message "!" "Chroot List File path has been updated." $YELLOW $MAIN_COLOR
+                ;;
+            7)  # LS Recurse Enable
+                value=$(grep -E '^ls_recurse_enable=' $FTP_CONFIG | cut -d= -f2)
+                if [ "$value" == "YES" ]; then
+                    sed -i 's/^ls_recurse_enable=YES/ls_recurse_enable=NO/' $FTP_CONFIG
+                    show_message "!" "LS Recurse Enable has been set to NO." $YELLOW $MAIN_COLOR
+                else
+                    sed -i 's/^ls_recurse_enable=NO/ls_recurse_enable=YES/' $FTP_CONFIG
+                    show_message "!" "LS Recurse Enable has been set to YES." $YELLOW $MAIN_COLOR
+                fi
+                ;;
+            8)  # Listen
+                value=$(grep -E '^listen=' $FTP_CONFIG | cut -d= -f2)
+                if [ "$value" == "YES" ]; then
+                    sed -i 's/^listen=YES/listen=NO/' $FTP_CONFIG
+                    show_message "!" "Listen has been set to NO." $YELLOW $MAIN_COLOR
+                else
+                    sed -i 's/^listen=NO/listen=YES/' $FTP_CONFIG
+                    show_message "!" "Listen has been set to YES." $YELLOW $MAIN_COLOR
+                fi
+                ;;
+            9)  # Listen IPv6
+                value=$(grep -E '^listen_ipv6=' $FTP_CONFIG | cut -d= -f2)
+                if [ "$value" == "YES" ]; then
+                    sed -i 's/^listen_ipv6=YES/listen_ipv6=NO/' $FTP_CONFIG
+                    show_message "!" "Listen IPv6 has been set to NO." $YELLOW $MAIN_COLOR
+                else
+                    sed -i 's/^listen_ipv6=NO/listen_ipv6=YES/' $FTP_CONFIG
+                    show_message "!" "Listen IPv6 has been set to YES." $YELLOW $MAIN_COLOR
+                fi
+                ;;
+            10)  # Use Localtime
+                value=$(grep -E '^use_localtime=' $FTP_CONFIG | cut -d= -f2)
+                if [ "$value" == "YES" ]; then
+                    sed -i 's/^use_localtime=YES/use_localtime=NO/' $FTP_CONFIG
+                    show_message "!" "Use Localtime has been set to NO." $YELLOW $MAIN_COLOR
+                else
+                    sed -i 's/^use_localtime=NO/use_localtime=YES/' $FTP_CONFIG
+                    show_message "!" "Use Localtime has been set to YES." $YELLOW $MAIN_COLOR
+                fi
+                ;;
+            11)  # Go Back
+                break
+                ;;
+            *)  # Invalid option
+                show_message "!" "Invalid option." $RED $MAIN_COLOR
+                ;;
+        esac
+    done
 }
 
 # Main menu loop
